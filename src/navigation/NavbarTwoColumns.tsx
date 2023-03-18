43ac827ac6c3694e28dad06287eb3183c92bd912
Anil Antony <anilantony3134@gmail.com>
2023-03-18 08:41:32 +0530

    fix: added package change 2

--- src/navigation/NavbarTwoColumns.tsx
+++ src/navigation/NavbarTwoColumns.tsx
@@ -7,32 +7,35 @@ type INavbarProps = {
   children: ReactNode;
 };
 
-const NavbarTwoColumns = (props: INavbarProps) => (
-  <div className="flex flex-wrap justify-between items-center">
-    <div>
-      <Link href="/">
-        <a>{props.logo}</a>
-      </Link>
+const NavbarTwoColumns = (props: INavbarProps) => {
+  console.log(props);
+  return (
+    <div className="flex flex-wrap justify-between items-center">
+      <div>
+        <Link href="/">
+          <a>{props.logo}</a>
+        </Link>
+      </div>
+
+      <nav>
+        <ul className="navbar flex items-center font-medium text-xl text-gray-800">
+          {props.children}
+        </ul>
+      </nav>
+
+      <style jsx>
+        {`
+          .navbar :global(li:not(:first-child)) {
+            @apply mt-0;
+          }
+
+          .navbar :global(li:not(:last-child)) {
+            @apply mr-5;
+          }
+        `}
+      </style>
     </div>
-
-    <nav>
-      <ul className="navbar flex items-center font-medium text-xl text-gray-800">
-        {props.children}
-      </ul>
-    </nav>
-
-    <style jsx>
-      {`
-        .navbar :global(li:not(:first-child)) {
-          @apply mt-0;
-        }
-
-        .navbar :global(li:not(:last-child)) {
-          @apply mr-5;
-        }
-      `}
-    </style>
-  </div>
-);
+  );
+};
 
 export { NavbarTwoColumns };

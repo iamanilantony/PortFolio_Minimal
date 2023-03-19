afcd9f39ee297552981a1819afecbf425214b93e
Anil Antony <anilantony3134@gmail.com>
2023-03-19 09:03:53 +0530

    fix: added package change

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -2,12 +2,10 @@ import { VerticalFeatureRow } from "../feature/VerticalFeatureRow";
 import { Section } from "../layout/Section";
 
 const VerticalFeatures = () => (
-  <Section
-    title="Your title here"
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada nisi tellus, non imperdiet nisi tempor at."
-  >
+  <Section>
     <VerticalFeatureRow
-      title="Your title here"
+      title="Anil Antony"
+      subtitle="Scaling at Settlin"
       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
       image="/assets/images/feature.svg"
       imageAlt="First feature alt text"

4be44ba2b8a63e80f9095ec53e03bac9f83bbfc8
Anil Antony <anilantony3134@gmail.com>
2023-03-20 08:40:09 +0530

    fix: added package changes

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -1,36 +1,37 @@
-import { AppConfig } from "../utils/AppConfig";
+import Link from "next/link";
+// import { AppConfig } from "../utils/AppConfig";
 
 type ILogoProps = {
   xl?: boolean;
 };
 
 const Logo = (props: ILogoProps) => {
-  const size = props.xl ? "44" : "32";
+  // const size = props.xl ? "44" : "32";
   const fontStyle = props.xl
     ? "font-semibold text-3xl"
     : "font-semibold text-xl";
 
   return (
     <span className={`text-gray-900 inline-flex items-center ${fontStyle}`}>
-      <svg
-        className="text-primary-500 stroke-current mr-1"
-        xmlns="http://www.w3.org/2000/svg"
-        width={size}
-        height={size}
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M0 0h24v24H0z" stroke="none" />
-        <rect x="3" y="12" width="6" height="8" rx="1" />
-        <rect x="9" y="8" width="6" height="12" rx="1" />
-        <rect x="15" y="4" width="6" height="16" rx="1" />
-        <path d="M4 20h14" />
-      </svg>
-
-      {AppConfig.site_name}
+      <nav>
+        <ul className="navbar flex items-center font-medium text-xl text-gray-800">
+          <li className="mr-2">
+            <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
+              <a> Home</a>
+            </Link>
+          </li>
+          <li className="mr-2">
+            <Link href="/">
+              <a>Works</a>
+            </Link>
+          </li>
+          <li className="mr-2">
+            <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
+              <a>Blogs</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </span>
   );
 };

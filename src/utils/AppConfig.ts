69116cf0b5d801cb227b340bee2864bd04b51bee
Anil Antony <anilantony3134@gmail.com>
2023-03-21 08:42:01 +0530

    fix: added package change 2

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: "Starter",
-  title: "Nextjs Starter",
+  site_name: "Anil Antony",
+  title: "Anil Antony",
   description: "Starter code for your Nextjs Boilerplate with Tailwind CSS",
   locale: "en",
 };

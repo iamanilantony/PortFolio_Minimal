3609b4b259576d64c03d3de37b1fa98f7238c131
Anil Antony <anilantony3134@gmail.com>
2023-03-22 09:03:40 +0530

    fix: added package change

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,33 +1,15 @@
-import Link from "next/link";
 import { Background } from "../background/Background";
-import { Button } from "../button/Button";
-import { HeroOneButton } from "../hero/HeroOneButton";
 import { Section } from "../layout/Section";
 import NavBar from "../navigation/NavBar";
+import { VerticalFeatures } from "./VerticalFeatures";
 
 const Hero = () => (
   <Background color="bg-gray-100">
     <Section yPadding="py-6">
       <NavBar />
     </Section>
-
-    <Section yPadding="pt-20 pb-32">
-      <HeroOneButton
-        title={
-          <>
-            {"The modern landing page for\n"}
-            <span className="text-primary-500">React developers</span>
-          </>
-        }
-        description="The easiest way to build a React landing page in seconds."
-        button={
-          <Link href="https://creativedesignsguru.com/category/nextjs/">
-            <a>
-              <Button xl>Download Your Free Theme</Button>
-            </a>
-          </Link>
-        }
-      />
+    <Section yPadding="pb-32">
+      <VerticalFeatures />
     </Section>
   </Background>
 );

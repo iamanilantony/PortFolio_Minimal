b058b70278c99357b46a4e1d554786b29a289294
Anil Antony <anilantony3134@gmail.com>
2023-03-22 09:03:17 +0530

    fix: added package change 2

--- src/layout/Section.tsx
+++ src/layout/Section.tsx
@@ -10,7 +10,7 @@ type ISectionProps = {
 const Section = (props: ISectionProps) => (
   <div
     className={`max-w-screen-lg mx-auto px-3 ${
-      props.yPadding ? props.yPadding : "py-16"
+      props.yPadding ? props.yPadding : "py-4"
     }`}
   >
     {(props.title || props.description) && (

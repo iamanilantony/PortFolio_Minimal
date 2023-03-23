d1495047643e808a59b1a27af0b3aa38052ef77a
Anil Antony <anilantony3134@gmail.com>
2023-03-23 08:46:02 +0530

    fix: added package change 2

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -4,24 +4,10 @@ import { Button } from "../button/Button";
 import { HeroOneButton } from "../hero/HeroOneButton";
 import { Section } from "../layout/Section";
 import NavBar from "../navigation/NavBar";
-// import { NavbarTwoColumns } from "../navigation/NavbarTwoColumns";
-// import { Logo } from "./Logo";
 
 const Hero = () => (
   <Background color="bg-gray-100">
     <Section yPadding="py-6">
-      {/* <NavbarTwoColumns logo={<Logo xl />}>
-        <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            <a>GitHub</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>Sign in</a>
-          </Link>
-        </li>
-      </NavbarTwoColumns> */}
       <NavBar />
     </Section>
 

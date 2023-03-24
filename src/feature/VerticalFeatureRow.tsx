d25a82e5c66fe7add9e6c9dcc1000e3c42bb5e4c
Anil Antony <anilantony3134@gmail.com>
2023-03-24 10:23:06 +0530

    fix: added changes

--- src/feature/VerticalFeatureRow.tsx
+++ src/feature/VerticalFeatureRow.tsx
@@ -1,19 +1,20 @@
 import className from "classnames";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { AiFillGithub, AiOutlineLink } from "react-icons/ai";
 
 type IVerticalFeatureRowProps = {
   title: string;
   subtitle?: string;
   description: string;
-  image: string;
+  image?: string;
   imageAlt: string;
   reverse?: boolean;
+  links?: boolean;
 };
 
 const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
   const verticalFeatureClass = className(
-    "mt-5",
     "flex",
     "justify-between",
     "flex-wrap",
@@ -27,22 +28,67 @@ const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
 
   return (
     <div className={verticalFeatureClass}>
-      <div className="w-full sm:w-1/2 text-left sm:px-6">
-        <h3 className="text-4xl text-gray-900 font-bold">{props.title}</h3>
-        <h5 className="text-xl">{props.subtitle}</h5>
-        <div className="mt-6 text-md leading-2">{props.description}</div>
+      <div className="w-full sm:w-1/2 text-left sm:px-2 flex-[2_2_0%]">
+        <h3 className="text-5xl text-gray-900 font-bold">{props.title}</h3>
+        <h5
+          className="text-lg font-normal text-gray-800"
+          style={{ marginTop: "-4px" }}
+        >
+          {props.subtitle}
+        </h5>
+        <div className="mt-6 text-md leading-1">{props.description}</div>
       </div>
 
-      <div className="flex justify-end w-full sm:w-1/2 p-6">
-        <Image
-          width="200"
-          height="200"
-          src={`${router.basePath}${props.image}`}
-          alt={props.imageAlt}
-        />
+      {props.image && (
+        <div className="flex justify-end w-full sm:w-1/2 p-6 flex-1">
+          <Image
+            width="200"
+            height="200"
+            src={`${router.basePath}${props.image}`}
+            alt={props.imageAlt}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+const VerticalFeatureCard = (props: IVerticalFeatureRowProps) => {
+  // const verticalFeatureClass = className(
+  //   "flex",
+  //   "w-52",
+  //   "h-64",
+  //   "mx-auto",
+  //   // "mt-10",
+  //   "bg-gradient-to-r",
+  //   "p-[6px]",
+  //   "from-[#6EE7B7]",
+  //   "via-[#3B82F6]",
+  //   "to-[#9333EA]",
+  //   {
+  //     "flex-row-reverse": props.reverse,
+  //   }
+  // );
+
+  // const router = useRouter();
+  return (
+    <div className="card w-56 glass text-gray-800">
+      <div className="card-body">
+        <h2 className="card-title">{props.title}</h2>
+        <p>{props.description}</p>
+        {props.links && (
+          <div className="flex text-xl">
+            <div className="pr-2">
+              <AiFillGithub />
+            </div>
+            <div>
+              <AiOutlineLink />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-export { VerticalFeatureRow };
+export { VerticalFeatureRow, VerticalFeatureCard };

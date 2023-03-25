6803844a5f8aba127f0276b1b8499c00f1efeeba
Anil Antony <anilantony3134@gmail.com>
2023-03-25 10:23:35 +0530

    fix: added changes 2

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -1,4 +1,7 @@
-import { VerticalFeatureRow } from "../feature/VerticalFeatureRow";
+import {
+  VerticalFeatureCard,
+  VerticalFeatureRow,
+} from "../feature/VerticalFeatureRow";
 import { Section } from "../layout/Section";
 
 const VerticalFeatures = () => (
@@ -9,14 +12,52 @@ const VerticalFeatures = () => (
       description="I strive while utilising both the logical and creative part of the brain. I feel most in my place while continuously operating in these roles as a full stack developer within a team."
       image="/assets/images/animoji.png"
       imageAlt="First feature alt text"
+      // style={{ marginTop: "-4rem" }}
     />
-    <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature2.svg"
-      imageAlt="Second feature alt text"
-      reverse
-    />
+    <Section title="Projects">
+      <div className="flex justify-between" style={{ marginTop: "-1rem" }}>
+        <VerticalFeatureCard
+          title="BizSol"
+          description="A Platform for companies and experts to meet for their major issues"
+          imageAlt="Second feature alt text"
+          links
+        />
+        <VerticalFeatureCard
+          title="BizSol"
+          description=" justo erat semper enim, non hendrerit dui odio id enim."
+          imageAlt="Second feature alt text"
+          links
+        />
+        <VerticalFeatureCard
+          title="BizSol"
+          description=" justo erat semper enim, non hendrerit dui odio id enim."
+          imageAlt="Second feature alt text"
+          links
+        />
+      </div>
+    </Section>
+    <Section title="Work Experience">
+      <div className="flex justify-between" style={{ marginTop: "-1rem" }}>
+        <VerticalFeatureCard
+          title="BizSol"
+          description="A Platform for companies and experts to meet for their major issues"
+          imageAlt="Second feature alt text"
+          links
+        />
+        <VerticalFeatureCard
+          title="BizSol"
+          description=" justo erat semper enim, non hendrerit dui odio id enim."
+          imageAlt="Second feature alt text"
+          links
+        />
+        <VerticalFeatureCard
+          title="BizSol"
+          description=" justo erat semper enim, non hendrerit dui odio id enim."
+          imageAlt="Second feature alt text"
+          links
+        />
+      </div>
+    </Section>
     <VerticalFeatureRow
       title="Your title here"
       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."

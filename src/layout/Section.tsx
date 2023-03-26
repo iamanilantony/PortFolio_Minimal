d51791ac9f56e02d70e4abb6503b6ca43d49e23b
Anil Antony <anilantony3134@gmail.com>
2023-03-26 22:09:54 +0530

    fix: added changes

--- src/layout/Section.tsx
+++ src/layout/Section.tsx
@@ -4,19 +4,20 @@ type ISectionProps = {
   title?: string;
   description?: string;
   yPadding?: string;
+  icon?: ReactNode;
   children: ReactNode;
 };
 
 const Section = (props: ISectionProps) => (
   <div
-    className={`max-w-screen-md mx-auto px-3 ${
+    className={`max-w-screen-md mx-auto ${
       props.yPadding ? props.yPadding : "py-12"
     }`}
   >
     {(props.title || props.description) && (
       <div className="mb-12 text-left">
         {props.title && (
-          <h2 className="text-4xl text-gray-900 font-bold">{props.title}</h2>
+          <h2 className="text-3xl text-gray-900 font-bold"> {props.title}</h2>
         )}
         {props.description && (
           <div className="mt-4 text-xl md:px-20">{props.description}</div>

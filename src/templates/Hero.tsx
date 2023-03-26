8679b610e29c8350bdb394e0442b80977eae1a64
Anil Antony <anilantony3134@gmail.com>
2023-03-26 22:10:06 +0530

    fix: added changes

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -3,15 +3,16 @@ import { Section } from "../layout/Section";
 import NavBar from "../navigation/NavBar";
 import { VerticalFeatures } from "./VerticalFeatures";
 
-const Hero = () => (
-  <Background color="bg-gray-100 dark:bg-gray-800 dark:text-white">
-    <Section yPadding="py-6">
-      <NavBar />
-    </Section>
-    <Section yPadding="pb-32">
-      <VerticalFeatures />
-    </Section>
-  </Background>
-);
-
+const Hero = () => {
+  return (
+    <Background color="bg-gray-100 dark:bg-gray-800 dark:text-white">
+      <Section yPadding="py-6">
+        <NavBar />
+      </Section>
+      <Section yPadding="pb-2">
+        <VerticalFeatures />
+      </Section>
+    </Background>
+  );
+};
 export { Hero };

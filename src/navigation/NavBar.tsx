b9555921f9f735f4e76f5abc65edded53bddd587
Anil Antony <anilantony3134@gmail.com>
2023-03-31 08:16:59 +0530

    fix: added fix

--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -1,50 +1,68 @@
 import Link from "next/link";
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
-const NavBar = () => (
-  <div className="navbar bg-base-800">
-    <div className="flex-1">
-      <a className="btn btn-ghost normal-case text-xl">Home</a>
-    </div>
-    <div className="flex-none">
-      <ul className="menu menu-horizontal px-1 hidden md:flex">
-        <li>
-          <Link as="/projects" href={"/projects"}>
-            <a>Projects</a>
-          </Link>
-        </li>
-        <li>
-          <a>Resume</a>
-        </li>
-        <li tabIndex={0}>
-          <a>
-            Social
-            <svg
-              className="fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
+export default function Navbar() {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  return (
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between py-3 mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a
+              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+              href="#pablo"
             >
-              <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-            </svg>
-          </a>
-          <ul className="p-2 bg-base-100">
-            <li>
-              <Link href="https://github.com/AnilAntony3134">
-                <a>Github</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="https://www.linkedin.com/in/iamanilantony/">
-                <a>LinkedIn</a>
-              </Link>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
-
-export default NavBar;
+              <Link href="/">Home</Link>
+            </a>
+            <button
+              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              {/* <i className="fas fa-bars"></i> */}
+              <GiHamburgerMenu />
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
+                  href="#pablo"
+                >
+                  <i className="fab fa-facebook-square text-lg leading-lg opacity-75"></i>
+                  <span className="ml-2">Resume</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
+                  href="#pablo"
+                >
+                  <i className="fab fa-twitter text-lg leading-lg opacity-75"></i>
+                  <span className="ml-2">Projects</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
+                  href="#pablo"
+                >
+                  <i className="fab fa-pinterest text-lg leading-lg opacity-75"></i>
+                  <span className="ml-2">links</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}

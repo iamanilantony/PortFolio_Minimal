4434c849cfba4241ce73fb28db68044e0b28eee2
Anil Antony <anilantony3134@gmail.com>
2023-04-15 08:33:06 +0530

    fix: added changes

--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -1,15 +1,15 @@
 import { useTheme } from "next-themes";
 import Link from "next/link";
 import React from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
+// import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Navbar() {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  // const [navbarOpen, setNavbarOpen] = React.useState(false);
   const { theme, setTheme } = useTheme();
   return (
     <>
       <nav className="relative flex flex-wrap items-center justify-between py-3 mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="container px-4 mx-auto flex items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
               className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
@@ -17,57 +17,25 @@ export default function Navbar() {
             >
               <Link href="/">Home</Link>
             </a>
-            <button
+            {/* <button
               className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              {/* <i className="fas fa-bars"></i> */}
-              <GiHamburgerMenu />
-            </button>
+            > */}
+            {/* <i className="fas fa-bars"></i> */}
+            {/* <GiHamburgerMenu /> */}
+            {/* </button> */}
           </div>
           <div
             className={
-              "lg:flex flex-grow items-center" +
-              (navbarOpen ? " flex" : " hidden")
+              "lg:flex flex-grow items-center flex"
+              // (navbarOpen ? " flex" : " hidden")
             }
             id="example-navbar-danger"
           >
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-              {/* <li className="nav-item">
-                <Link href="https://drive.google.com/file/d/1viRhQqaLC3VEV0xL9wyMaqy0hrkdEbOW/view?usp=sharing">
-                  <a
-                    className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
-                    href="#pablo"
-                  >
-                    <i className="fab fa-facebook-square text-lg leading-lg opacity-75"></i>
-                    <span className="ml-2">Resume</span>
-                  </a>
-                </Link>
-              </li>
               <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
-                  href="#pablo"
-                >
-                  <i className="fab fa-twitter text-lg leading-lg opacity-75"></i>
-                  <span className="ml-2">Projects</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
-                  href="#pablo"
-                >
-                  <i className="fab fa-pinterest text-lg leading-lg opacity-75"></i>
-                  <span className="ml-2">links</span>
-                </a>
-              </li> */}
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
-                  href="#pablo"
-                >
+                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75">
                   <i className="fab fa-pinterest text-lg leading-lg opacity-75"></i>
                   <span className="ml-2">
                     <input

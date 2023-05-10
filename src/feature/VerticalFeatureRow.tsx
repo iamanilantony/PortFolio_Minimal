9accb34940ff9fca41c2b7f30af095a6b4653820
Anil Antony <anilantony3134@gmail.com>
2023-05-10 21:45:59 +0530

    fix: added analytics

--- src/feature/VerticalFeatureRow.tsx
+++ src/feature/VerticalFeatureRow.tsx
@@ -101,25 +101,22 @@ const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
         {props.count && (
           <div className="text-md leading-1 flex items-center mt-4">
             <div className="pr-4 cursor-pointer">
-              <Link
-                href="https://www.linkedin.com/in/iamanilantony/"
-                target="_blank"
-              >
+              <Link href="https://www.linkedin.com/in/iamanilantony/">
                 <BsLinkedin />
               </Link>
             </div>
             <div className="pr-4 cursor-pointer">
-              <Link href="https://github.com/iamanilantony " target="_blank">
+              <Link href="https://github.com/iamanilantony">
                 <BsGithub />
               </Link>
             </div>
             <div className="pr-4 cursor-pointer">
-              <Link href="https://twitter.com/iamanilantony" target="_blank">
+              <Link href="https://twitter.com/iamanilantony">
                 <BsTwitter />
               </Link>
             </div>
             <div className="pr-4 cursor-pointer">
-              <Link href="https://instagram.com/iamanilantony" target="_blank">
+              <Link href="https://instagram.com/iamanilantony">
                 <BsInstagram />
               </Link>
             </div>

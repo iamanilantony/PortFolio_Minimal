98b4f6af0af03a4262836afda5f816fd557912c5
Anil Antony <anilantony3134@gmail.com>
2023-05-10 21:45:51 +0530

    fix: added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { AppProps } from "next/app";
 import "../styles/global.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider attribute="class">
     <Component {...pageProps} />
+    <Analytics />
   </ThemeProvider>
 );
 

cad8cb411361ec87ab7d2193a759a992ff1e08f6
Anil Antony <anilantony3134@gmail.com>
2023-06-21 08:50:12 +0530

    feat: card redo

--- src/layout/Section.tsx
+++ src/layout/Section.tsx
@@ -8,23 +8,33 @@ type ISectionProps = {
   children: ReactNode;
 };
 
-const Section = (props: ISectionProps) => (
-  <div
-    className={`max-w-screen-md mx-auto px-2 md:px-0 ${
-      props.yPadding ? props.yPadding : "py-12"
-    }`}
-  >
-    {(props.title || props.description) && (
-      <div className="mb-12 text-left">
-        {props.title && <h2 className="text-3xl font-bold"> {props.title}</h2>}
-        {props.description && (
-          <div className="mt-4 text-xl md:px-20">{props.description}</div>
-        )}
-      </div>
-    )}
+const Section = (props: ISectionProps) => {
+  console.log(props.icon, "anil");
+  return (
+    <div
+      className={`max-w-screen-md mx-auto px-2 md:px-0 ${
+        props.yPadding ? props.yPadding : "py-12"
+      }`}
+    >
+      {(props.title || props.description) && (
+        <div className="mb-12 text-left">
+          {props.title && (
+            <span className="flex items-center">
+              {props.icon && (
+                <span className="text-2xl mr-2">{props.icon}</span>
+              )}
+              <h2 className="text-3xl font-bold"> {props.title}</h2>
+            </span>
+          )}
+          {props.description && (
+            <div className="mt-4 text-xl md:px-20">{props.description}</div>
+          )}
+        </div>
+      )}
 
-    {props.children}
-  </div>
-);
+      {props.children}
+    </div>
+  );
+};
 
 export { Section };
